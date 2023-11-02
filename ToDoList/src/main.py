0f517bcac91c15cac05a136ffbe581e291b02717
Britney Abner <brodytabner@protonmail.com>
2023-11-02 11:11:25 -0500

    uncommented lines that test user input

--- ToDoList/src/main.py
+++ ToDoList/src/main.py
@@ -4,15 +4,15 @@ import IOFunctions
 
 
 def main():
-    # IOFunctions.write_list(
-    #        TaskList.add_task(
-    #            IOFunctions.read_list(),
-    #            Task.create_task(
-    #                IOFunctions.get_name_input(),
-    #                IOFunctions.get_date_input()
-    #                )
-    #            )
-    #        )
+    IOFunctions.write_list(
+            TaskList.add_task(
+                IOFunctions.read_list(),
+                Task.create_task(
+                    IOFunctions.get_name_input(),
+                    IOFunctions.get_date_input()
+                    )
+                )
+            )
     IOFunctions.print_task_list(IOFunctions.read_list())
 
 
